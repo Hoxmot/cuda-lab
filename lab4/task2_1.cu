4ca6cdde40f1083acee5319d90fe4c882d9e079c
Hoxmot <hoxmot@gmail.com>
2019-03-28 22:30:30 +0100

    [Lab 4] - task 2 implementation

--- lab4/task2_1.cu
+++ lab4/task2_1.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cuda_runtime_api.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ __global__ void add_vec(int *v1, int *v2, int *res, size_t l) {
     int step = gridDim.x * blockDim.x;
 
     for (; i < l; i+= step) {
-	res[i] = v1[i] + v2[i];
+	    res[i] = v1[i] + v2[i];
     }
 
 }
@@ -22,19 +23,22 @@ __global__ void mul(int *m, int *v, int *res, size_t l) {
     int step = gridDim.x * blockDim.x;
     int k;
     for (; i < l * l; i += step) {
-	k = i / l;
-	res[i] = m[i] * v[k];
+	    k = i / l;
+	    res[i] = m[i] * v[k];
     }
 }
 
 int main() {
-    int m_cpu[LEN * LEN], v_cpu[LEN], res_cpu[LEN * LEN];
+    int *m_cpu, *v_cpu, *res_cpu;
+
+    m_cpu = (int*) calloc(LEN * LEN, sizeof(int));
+    v_cpu = (int*) calloc(LEN, sizeof(int));
 
     for (int i = 0; i < LEN; i++) {
-	for (int k = 0; k < LEN; k++) {
-	    m_cpu[i * LEN + k] = i * k + 42;
-	}
-	v_cpu[i] = i * 40 + 2;
+	    for (int k = 0; k < LEN; k++) {
+	        m_cpu[i * LEN + k] = i * k + 42;
+	    }
+	    v_cpu[i] = i * 40 + 2;
     }
 
     cudaError_t status;
@@ -42,52 +46,59 @@ int main() {
 
     status = cudaMalloc((void**)&m_gpu, sizeof(int) * LEN * LEN);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaMalloc((void**)&v_gpu, sizeof(int) * LEN);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaMalloc((void**)&res_gpu, sizeof(int) * LEN * LEN);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
 
     status = cudaMemcpy(m_gpu, m_cpu, sizeof(int) * LEN * LEN, cudaMemcpyHostToDevice);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaMemcpy(v_gpu, v_cpu, sizeof(int) * LEN, cudaMemcpyHostToDevice);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
+
+    free(m_cpu);
+    free(v_cpu);
 
     add_vec<<<2, 10, 0>>>(m_gpu, v_gpu, res_gpu, LEN);
  
+    res_cpu = (int*) calloc(LEN * LEN, sizeof(int));
+
     status = cudaMemcpy(res_cpu, res_gpu, sizeof(int) * LEN * LEN, cudaMemcpyDeviceToHost);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
 
     status = cudaFree(m_gpu);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaFree(v_gpu);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaFree(res_gpu);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
 
     for (int i = 0; i < LEN; i++) {
-	for (int k = 0; k < LEN; k++) {
-	    cout << res_cpu[i * LEN + k] << " ";
-	}
-	cout << endl;
+	    for (int k = 0; k < LEN; k++) {
+	        cout << res_cpu[i * LEN + k] << " ";
+	    }
+	    cout << endl;
     }
+
+    free(res_cpu);
 
     return 0;
 
--- lab4/task2_2.cu
+++ lab4/task2_2.cu
@@ -12,7 +12,7 @@ __global__ void add_vec(int *v1, int *v2, int *res, size_t l) {
     int step = gridDim.x * blockDim.x;
 
     for (; i < l; i+= step) {
-	res[i] = v1[i] + v2[i];
+	    res[i] = v1[i] + v2[i];
     }
 
 }
@@ -22,69 +22,74 @@ __global__ void mul(int *m, int *v, int *res, size_t l) {
     int step = gridDim.x * blockDim.x;
     int k;
     for (; i < l * l; i += step) {
-	k = i / l;
-	res[i] = m[i] * v[k];
+	    k = i / l;
+	    res[i] = m[i] * v[k];
+    }
+}
+
+__global__ void gen_numbers(int *m, int *v, size_t l) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int step = gridDim.x * blockDim.x;
+    int k;
+    for (; i < l * l; i += step) {
+        m[i] = i + 42;
+        if (i % l == 0) {
+            k = i / l;
+            v[k] = k * 40 + 2;
+        }
+    }
+}
+
+__global__ void print_numbers(int *res, size_t l) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int step = gridDim.x * blockDim.x;
+    for (; i < l * l; i += step) {
+        printf("%d ", res[i]);
+        if (i % l == l - 1) {
+            printf("\n");
+        }
     }
 }
 
 int main() {
-    int m_cpu[LEN * LEN], v_cpu[LEN], res_cpu[LEN * LEN];
-
-    for (int i = 0; i < LEN; i++) {
-	    m_cpu[i] = i;
-	    v_cpu[i] = i * 40 + 2;
-    }
-
     cudaError_t status;
     int *m_gpu, *v_gpu, *res_gpu;
 
     status = cudaMalloc((void**)&m_gpu, sizeof(int) * LEN * LEN);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaMalloc((void**)&v_gpu, sizeof(int) * LEN);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaMalloc((void**)&res_gpu, sizeof(int) * LEN * LEN);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
 
-    status = cudaMemcpy(m_gpu, m_cpu, sizeof(int) * LEN * LEN, cudaMemcpyHostToDevice);
-    if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
-    }
-    status = cudaMemcpy(v_gpu, v_cpu, sizeof(int) * LEN, cudaMemcpyHostToDevice);
-    if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
-    }
+    gen_input<<<2, 10, 0>>>(m_gpu, v_gpu, LEN);
 
     add_vec<<<2, 10, 0>>>(m_gpu, v_gpu, res_gpu, LEN);
  
+    print_numbers<<<2, 10, 0>>>(res_gpu, l);
+
     status = cudaMemcpy(res_cpu, res_gpu, sizeof(int) * LEN * LEN, cudaMemcpyDeviceToHost);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
 
     status = cudaFree(m_gpu);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaFree(v_gpu);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
     status = cudaFree(res_gpu);
     if (status != cudaSuccess) {
-	cout << cudaGetErrorString(status) << endl;
-    }
-
-    for (int i = 0; i < LEN; i++) {
-	for (int k = 0; k < LEN; k++) {
-	    cout << res_cpu[i * LEN + k] << " ";
-	}
-	cout << endl;
+	    cout << cudaGetErrorString(status) << endl;
     }
 
     return 0;
